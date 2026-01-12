0364adecf5bfa4c705c5d8763a816c68ee49e28f
dottgiovannivetere-design <dottgiovannivetere@gmail.com>
2026-01-12 16:38:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('fetch', e => {});
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// NESSUNA CACHE: evita versioni vecchie e problemi col PDF
+self.addEventListener("fetch", () => {});
